98cf31601bbcaabc6ec603fb0ad9c5c059df8cb7
silver380 <r.rastegari380@outlook.com>
2023-05-12 03:54:26 +0430

    ready for the training

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
+from torch.utils.data import Dataset
 import torch
-import torch.nn as nn
+from torchvision import datasets, transforms
+from evolutionary import EvolutionaryAlgorithm
 import numpy as np
-import torchvision
-from torchvision import *
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
-from torchvision.models import resnet34, resnet18, vgg11
-from sklearn.metrics import confusion_matrix, f1_score
-import random
-import torch.optim as optim
-import torch.nn.functional as F
-import numpy as np
-import torchvision
-import matplotlib.pyplot as plt
-import math
-from tqdm import tqdm
-import util
-epohcs = 10
-n_iter = 10 #util.n_iter
-batch_size = 32
+import sys
+
+EPOCHS = 5
+NUM_TEST = 5
+N_ITER = 10
+POPULATION_SIZE = 10
+MUT_PROB = 0.9
+RECOMB_PROB = 0.5
+
+TRAIN_DATA_BATCH_SIZE = 4
+TEST_DATA_BATCH_SIZE = 4
+BATCH_SIZE = 32
+IMAGE_RSIZE = 128
+
+histories = []
 transform = transforms.Compose([
-    transforms.Resize((64, 64)), # Resize images
+    transforms.Resize((IMAGE_RSIZE, IMAGE_RSIZE)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                          std=[0.229, 0.224, 0.225])
-])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-train_data = datasets.CIFAR10('data', train=True,
-                              download=True, transform=transform)
-train_dataloader = DataLoader(train_data, batch_size=batch_size,shuffle=True )
+train_data = datasets.CIFAR10('data', train=True, download=True, transform=transform)
+test_data = datasets.CIFAR10('data', train=False, download=True, transform=transform)
 
-#loading the test data
-test_data = datasets.CIFAR10('data', train=False,
-                             download=True, transform=transform)
-test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False )
+image_datasets = {'train': train_data, 'val': test_data}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=BATCH_SIZE, shuffle=True) for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+if __name__ == "__main__":
+
+    ea = EvolutionaryAlgorithm(N_ITER, MUT_PROB, RECOMB_PROB, POPULATION_SIZE, EPOCHS, NUM_TEST, dataloaders, dataset_sizes)
+    best_ans, history = ea.run()
+    histories.append(history)
+    histories = np.array(histories)   
+    np.savetxt("histories.csv", histories,
+                delimiter = ",")
+
+    print(best_ans.fitness)
+
+    original_stdout = sys.stdout
+    with open('ans.txt', 'w') as f:
+                    sys.stdout = f
+                    bes_ext = "vgg11"
+                    if best_ans.net['extractor'] == 2:
+                            bes_ext = "resnet34"
+                    elif best_ans.net['extractor'] == 3:
+                            bes_ext = "resnet18"
+                    print(f"extractor for the best model is {bes_ext}")
+                    print(f"number of added hidden layers: {len(best_ans.net['mlp'])}")
+                    if len(best_ans.net['mlp']) > 0:
+                            for i in range(best_ans.net['mlp']):
+                                    print(f"layer {i+1} neurons: {best_ans.net['mlp'][i][0]}")
+                                    act =  "ReLU" if best_ans.net['mlp'][i][1] == 1 else "Sigmoid"
+                                    print(f"layer {i+1} activation function: {act}")
+                                    
+                    sys.stdout = original_stdout
